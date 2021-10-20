8bf839e874bb796d220bfb841c3524a197db584a
faris <faris@mail.com>
2021-10-20 00:22:20 -0700

    add dashboard validation

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
@@ -9,7 +10,11 @@ router.get("/", (req, res, next) => {
    });
 });
 
-router.post("/", (req, res, next) => {
+router.post("/", body("text").notEmpty().trim(), (req, res, next) => {
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+   }
    return res.json({
       taskText: req.body.task,
    });

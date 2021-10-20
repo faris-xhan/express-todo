66bf4c90eaa656d8667311d185bb385d0407f824
faris <faris@mail.com>
2021-10-20 00:17:20 -0700

    add server side validation for registeration

--- routes/signup.js
+++ routes/signup.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
 /* GET users listing. */
 router.get("/", (req, res, next) => {
    res.render("signup", { tabTitle: "TODO - Registeration" });
 });
 
-router.post("/", (req, res, next) => {
-   return res.json(req.body);
-});
+router.post(
+   "/",
+   body("fname").notEmpty().isLength({ min: 2 }),
+   body("lname").notEmpty().isLength({ min: 2 }),
+   body("email").notEmpty().isEmail().normalizeEmail(),
+   body("password").notEmpty().isLength({ min: 8 }),
+   (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+         return res.status(400).json({
+            status: 400,
+            message: "Please provide correct information.",
+         });
+      }
+      return res.json(req.body);
+   }
+);
 
 module.exports = router;

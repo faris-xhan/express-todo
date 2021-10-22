cdc5ffe0ce3438f90c8fcb7edaa4eefc32a8676e
faris <faris@mail.com>
2021-10-22 02:32:23 -0700

    refactor: add emptyOrRows helper function to check for empty data :hammer:

---
+++ helper.js
@@ -0,0 +1,5 @@
+const emptyOrRows = (rows) => rows || [];
+
+module.exports = {
+   emptyOrRows,
+};
--- services/db.js
+++ services/db.js
@@ -1,11 +1,12 @@
 const config = require("../config");
+const helper = require("../helper");
 const mysql = require("mysql2/promise");
 
 const query = async (sql, params) => {
    const connection = await mysql.createConnection(config.db);
    const [result] = await connection.execute(sql, params);
 
-   return result;
+   return helper.emptyOrRows(result);
 };
 
 module.exports = {

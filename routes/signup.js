f346979d98b0e35ea7436fcd95bbf0ed37fd3710
faris <faris@mail.com>
2021-10-22 03:24:24 -0700

    database: connect singup page with database :card_file_box:

--- routes/signup.js
+++ routes/signup.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const { createUser } = require("../services/userSerivce");
 
 /* GET users listing. */
 router.get("/", (req, res, next) => {
@@ -13,7 +14,7 @@ router.post(
    body("lname").notEmpty().isLength({ min: 2 }).trim(),
    body("email").notEmpty().isEmail().normalizeEmail().trim(),
    body("password").notEmpty().isLength({ min: 8 }),
-   (req, res, next) => {
+   async (req, res, next) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
          return res.status(400).json({
@@ -21,7 +22,28 @@ router.post(
             message: "Please provide correct information.",
          });
       }
-      return res.json(req.body);
+      const result = await createUser(
+         req.body.fname,
+         req.body.lname,
+         req.body.email,
+         req.body.password
+      );
+
+      switch (result) {
+         case "USER_CREATED":
+            return res.json({ message: "User created succesfully" });
+            break;
+         case "USER_EXIST":
+            return res
+               .status(400)
+               .json({ message: "Email exist please user another email." });
+            break;
+         default:
+            return res.status(500).json({
+               message: "Server side error has accord please try again",
+            });
+            break;
+      }
    }
 );
 
--- services/userSerivce.js
+++ services/userSerivce.js
@@ -9,6 +9,24 @@ const getUser = async (email, password) => {
    return user;
 };
 
+const createUser = async (fname, lname, email, password) => {
+   let result = "";
+   try {
+      const row = await db.query(
+         `INSERT INTO users(first_name, last_name, email, password) VALUES (?, ?, ?, ?)`,
+         [fname, lname, email, password]
+      );
+
+      result = "USER_CREATED";
+   } catch (error) {
+      result =
+         error.code === "ER_DUP_ENTRY" ? "USER_EXIST" : "SERVER_SIDE_ERROR";
+   }
+
+   return result;
+};
+
 module.exports = {
    getUser,
+   createUser,
 };

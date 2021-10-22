1a5dd3395efa848559d704417be028aad351aadc
faris <faris@mail.com>
2021-10-22 01:43:16 -0700

    database: add database connection and query service :card_file_box:

---
+++ services/db.js
@@ -0,0 +1,13 @@
+const config = require("../config");
+const mysql = require("mysql2/promise");
+
+const query = async (sql, params) => {
+   const connection = await mysql.createConnection(config.db);
+   const [result] = await connection.execute(sql, params);
+
+   return result;
+};
+
+module.exports = {
+   query,
+};
